e549ff4171d30c20b1784633bb4d137ce9948722
ljy <>
2017-12-10 15:35:42 +0900

    add > eslint, sass, hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
-const ENTRY_PATH = './src/index.js';
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const MODULES_PATH = '/node_modules';
-const OUTPUT = {
-    PATH: __dirname + '/public/',
-    FILENAME: 'bundle.js'
+const SOURCE_PATH = path.join(__dirname, 'src');
+
+const SOURCE = {
+    ENTRY: path.join(SOURCE_PATH, 'index.js'),
+    TEMPLATE: path.join(SOURCE_PATH, 'index.html'),
 };
+
+const OUTPUT = {
+    PATH: path.join(__dirname, 'dist'),
+    FILENAME: 'bundle.js',
+};
+
 const DEV_SERVER_PORT = 7000;
 
 module.exports = {
-    entry: ENTRY_PATH,
+    entry: SOURCE.ENTRY,
     output: {
         path: OUTPUT.PATH,
-        filename: OUTPUT.FILENAME
+        filename: OUTPUT.FILENAME,
     },
     devServer: {
         inline: true,
+        hot: true,
         port: DEV_SERVER_PORT,
-        contentBase: OUTPUT.PATH
+        contentBase: OUTPUT.PATH,
     },
     module: {
         loaders: [
@@ -23,17 +36,41 @@ module.exports = {
                 enforce: 'pre',
                 test: /\.js$/,
                 loader: 'eslint-loader',
-                exclude: MODULES_PATH
+                exclude: MODULES_PATH,
             },
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: MODULES_PATH,
                 query: {
-                    cacheDirectory: true, 
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+                    cacheDirectory: true,
+                    presets: ['react', ['env', { module: false }]],
+                },
+            },
+            {
+                test: /\.css$/,
+                // loader: ['style-loader', 'css-loader'],
+                loader: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                })),
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+            },
+        }),
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+            template: SOURCE.TEMPLATE,
+        }),
+    ],
 };
