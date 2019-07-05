7ecc08af72b583fb47fbe559cbcf69559b168543
JeongYoung Lee <mojomoth@JeongYoungui-MacBook-Pro.local>
2019-07-05 23:29:45 +0900

    initialize boilerplate DOM

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    const hello = 'Hello React JS';
+    const hello = 'Hello React Boilerplate';
     return (<h1>{hello}</h1>);
   }
 }
